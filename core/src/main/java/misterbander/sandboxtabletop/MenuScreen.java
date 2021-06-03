c3f5300fa902f8123ab5eb7ac859d9e28fa60d17
misterbander <61056749+misterbander@users.noreply.github.com>
2021-06-04 02:38:03 +0800

    Add logo and menu buttons

--- core/src/main/java/misterbander/sandboxtabletop/MenuScreen.java
+++ core/src/main/java/misterbander/sandboxtabletop/MenuScreen.java
@@ -1,11 +1,65 @@
 package misterbander.sandboxtabletop;
 
-import misterbander.gframework.GScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
-public class MenuScreen extends GScreen<SandboxTabletop>
+import space.earlygrey.shapedrawer.ShapeDrawer;
+
+public class MenuScreen extends SandboxTabletopScreen
 {
+	private final Texture logo = game.skin.get("logo", Texture.class);
+	
+	private final Color backgroundAccentColor = new Color(0xBA00A1FF);
+	
 	public MenuScreen(SandboxTabletop game)
 	{
 		super(game);
+		logo.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+	}
+	
+	@Override
+	public void show()
+	{
+		super.show();
+		Table table = new Table();
+		table.setFillParent(true);
+		table.add(new Image(logo)).top().pad(16);
+		
+		Table menuTable = new Table();
+		TextButton playButton = new TextButton("Play", game.skin, "textbuttonstyle");
+		playButton.addListener(new ChangeListener(() -> {}));
+		TextButton quitButton = new TextButton("Quit", game.skin, "textbuttonstyle");
+		quitButton.addListener(new ChangeListener(() -> Gdx.app.exit()));
+		menuTable.add(playButton).padBottom(16).row();
+		menuTable.add(quitButton);
+		
+		table.row();
+		table.add(menuTable).expand();
+		
+		stage.addActor(table);
+	}
+	
+	@Override
+	public void resize(int width, int height)
+	{
+		viewport.update(width, height, true);
+	}
+	
+	@Override
+	public void clearScreen()
+	{
+		super.clearScreen();
+		PolygonSpriteBatch batch = game.getBatch();
+		ShapeDrawer shapeDrawer = game.getShapeDrawer();
+		batch.begin();
+		shapeDrawer.setColor(backgroundAccentColor);
+		shapeDrawer.filledRectangle(80, 96, viewport.getWorldWidth() - 96, viewport.getWorldHeight() - 96, 4*MathUtils.degRad);
+		batch.end();
 	}
 }
--- core/src/main/java/misterbander/sandboxtabletop/SandboxTabletop.java
+++ core/src/main/java/misterbander/sandboxtabletop/SandboxTabletop.java
@@ -2,6 +2,16 @@ package misterbander.sandboxtabletop;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 import misterbander.gframework.GFramework;
 
@@ -10,6 +20,12 @@ import misterbander.gframework.GFramework;
  */
 public class SandboxTabletop extends GFramework
 {
+	// Fonts
+	public BitmapFont jhengheiui, jhengheiuiMini, jhengheiuiMax;
+	
+	// Skins
+	public final Skin skin = new Skin();
+	
 	@Override
 	public void create()
 	{
@@ -17,6 +33,44 @@ public class SandboxTabletop extends GFramework
 		Gdx.graphics.setContinuousRendering(false);
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		
+		// Load assets
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/msjhl.ttc"));
+		jhengheiui = generator.generateFont(generateFontParameter(40));
+		FreeTypeFontGenerator miniGenerator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/msjhl.ttc"));
+		jhengheiuiMini = miniGenerator.generateFont(generateFontParameter(25));
+		FreeTypeFontGenerator maxGenerator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/msjhl.ttc"));
+		jhengheiuiMax = maxGenerator.generateFont(generateFontParameter(50));
+		
+		AssetManager assetManager = getAssetManager();
+		assetManager.load("textures/logo.png", Texture.class);
+		assetManager.load("textures/gui.atlas", TextureAtlas.class);
+		assetManager.load("sounds/click.wav", Sound.class);
+		assetManager.finishLoading();
+		Gdx.app.log("SandboxTabletop | INFO", "Finished loading assets!");
+		
+		// Initialize skin
+		skin.add("logo", assetManager.get("textures/logo.png", Texture.class));
+		skin.addRegions(assetManager.get("textures/gui.atlas", TextureAtlas.class));
+		skin.add("infolabelstyle", new Label.LabelStyle(jhengheiui, Color.WHITE));
+		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+		textButtonStyle.up = skin.getDrawable("button");
+		textButtonStyle.over = skin.getDrawable("buttonover");
+		textButtonStyle.down = skin.getDrawable("buttondown");
+		textButtonStyle.font = jhengheiui;
+		textButtonStyle.fontColor = Color.WHITE;
+		textButtonStyle.downFontColor = Color.BLACK;
+		skin.add("textbuttonstyle", textButtonStyle);
+		
 		setScreen(new MenuScreen(this));
+	}
+	
+	private FreeTypeFontGenerator.FreeTypeFontParameter generateFontParameter(int size)
+	{
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.minFilter = Texture.TextureFilter.Linear;
+		parameter.magFilter = Texture.TextureFilter.Linear;
+		parameter.incremental = true;
+		parameter.size = size;
+		return parameter;
 	}
 }
